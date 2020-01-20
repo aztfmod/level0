e0ca9049604ea71431a97cc1617183c6729d656b
lolorol <lalesle@microsoft.com>
2020-01-20 16:39:44 +0000

    fixing application_id

--- launchpads/launchpad_opensource/keyvault.tf
+++ launchpads/launchpad_opensource/keyvault.tf
@@ -47,9 +47,9 @@ resource "azurerm_key_vault" "launchpad" {
       ]
     }
 
-        access_policy {
+      access_policy {
       tenant_id       = data.azurerm_client_config.current.tenant_id
-      object_id       = azuread_service_principal.launchpad.object_id
+      object_id       = azuread_service_principal.launchpad.application_id
 
       key_permissions = []
 
