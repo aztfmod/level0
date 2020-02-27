a18ce5ab70a7c4712828229a94a9b83f18b23d31
lolorol <lalesle@microsoft.com>
2020-02-27 21:39:25 +0000

    management group contributor

--- launchpads/launchpad_opensource_light/management_group_role.tf
+++ launchpads/launchpad_opensource_light/management_group_role.tf
@@ -1,23 +1,23 @@
-resource "azurerm_role_definition" "mgmt_role1" {
-  name               = "management-group"
-  scope              = data.azurerm_subscription.primary.id
+# resource "azurerm_role_definition" "mgmt_role1" {
+#   name               = "management-group"
+#   scope              = data.azurerm_subscription.primary.id
 
-  permissions {
-    actions     = [
-      "Microsoft.Management/managementGroups/read",
-      "Microsoft.Authorization/roleDefinitions/write"
-    ]
-    not_actions = []
-  }
+#   permissions {
+#     actions     = [
+#       "Microsoft.Management/managementGroups/read",
+#       "Microsoft.Authorization/roleDefinitions/write"
+#     ]
+#     not_actions = []
+#   }
 
-  assignable_scopes = [
-    "/providers/Microsoft.Management/managementGroups/",
-    data.azurerm_subscription.primary.id
-  ]
-}
+#   assignable_scopes = [
+#     "/providers/Microsoft.Management/managementGroups/",
+#     data.azurerm_subscription.primary.id
+#   ]
+# }
 
 resource "azurerm_role_assignment" "mgmt_role1" {
-  scope              = "/providers/Microsoft.Management/managementGroups/"
-  role_definition_id = azurerm_role_definition.mgmt_role1.id
-  principal_id       = azuread_service_principal.launchpad.object_id
+  scope                 = "/providers/Microsoft.Management/managementGroups/"
+  role_definition_name  = "Management Group Contributor"
+  principal_id          = azuread_service_principal.launchpad.object_id
 }
