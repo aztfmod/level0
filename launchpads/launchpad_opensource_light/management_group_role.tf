37b3913154366ae9f2dbfbf56d98cefd770d8256
lolorol <lalesle@microsoft.com>
2020-03-18 06:38:47 +0000

    Removing management groups for AIRS

--- launchpads/launchpad_opensource_light/management_group_role.tf
+++
@@ -1,8 +0,0 @@
-#
-# Grant permissions to the launchpad application to create management groups in the tenant
-#
-resource "azurerm_role_assignment" "management_group_contributor" {
-  scope                 = "/providers/Microsoft.Management/managementGroups/${data.azurerm_client_config.current.tenant_id}"
-  role_definition_name  = "Management Group Contributor"
-  principal_id          = azuread_service_principal.launchpad.id
-}
