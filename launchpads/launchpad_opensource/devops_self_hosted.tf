899375f56944de2d2a4defbb3cb3f71f260f9331
lolorol <lalesle@microsoft.com>
2020-04-07 10:18:15 +0000

    disabled ssh tty

--- launchpads/launchpad_opensource/devops_self_hosted.tf
+++ launchpads/launchpad_opensource/devops_self_hosted.tf
@@ -65,7 +65,7 @@ echo "    StrictHostKeyChecking no" >> ~/.ssh/config
 EOT
 
     docker_ssh_command = <<EOT
-ssh -tt -l ${module.blueprint_devops_self_hosted_agent.object.admin_username} ${module.blueprint_devops_self_hosted_agent.object.public_ip_address} <<EOF
+ssh -T -i ~/.ssh/${module.blueprint_devops_self_hosted_agent.object.public_ip_address}.private -l ${module.blueprint_devops_self_hosted_agent.object.admin_username} ${module.blueprint_devops_self_hosted_agent.object.public_ip_address} <<EOF
     az login --identity
     az acr login --name ${module.blueprint_container_registry.object.admin_username}
     docker pull ${module.blueprint_container_registry.object.login_server}/devops
