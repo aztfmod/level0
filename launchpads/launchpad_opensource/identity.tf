029f103bc29cbfb98e942aa7be4ed9df6cb0671b
lolorol <lalesle@microsoft.com>
2020-04-07 15:34:42 +0000

    comment heredoc

--- launchpads/launchpad_opensource/identity.tf
+++ launchpads/launchpad_opensource/identity.tf
@@ -46,54 +46,54 @@ locals {
   microsoft_graph_id                = "00000003-0000-0000-c000-000000000000"
   microsoft_graph_AppRoleAssignment_ReadWrite_All = "06b708a9-e830-4db3-a914-8e69da51d44f"
 
-  grant_admin_concent_command = <<EOT
-  set -e
+#   grant_admin_concent_command = <<EOT
+#   set -e
 
-function grant_consent {
+# function grant_consent {
 
-    resourceId=$1
-    principalId=$2
-    appRoleId=$3
+#     resourceId=$1
+#     principalId=$2
+#     appRoleId=$3
 
-    URI=$(echo  "https://graph.microsoft.com/beta/servicePrincipals/${resourceId}/appRoleAssignments") && echo " - uri: $URI"
+#     URI=$(echo  "https://graph.microsoft.com/beta/servicePrincipals/${resourceId}/appRoleAssignments") && echo " - uri: $URI"
 
-    # grant consent (Application.ReadWrite.OwnedBy)
-    JSON=$( jq -n \
-                --arg principalId "${principalId}" \
-                --arg resourceId "${resourceId}" \
-                --arg appRoleId "${appRoleId}" \
-            '{principalId: $principalId, resourceId: $resourceId, appRoleId: $appRoleId}' ) && echo " - body: $JSON"
+#     # grant consent (Application.ReadWrite.OwnedBy)
+#     JSON=$( jq -n \
+#                 --arg principalId "${principalId}" \
+#                 --arg resourceId "${resourceId}" \
+#                 --arg appRoleId "${appRoleId}" \
+#             '{principalId: $principalId, resourceId: $resourceId, appRoleId: $appRoleId}' ) && echo " - body: $JSON"
 
-    az rest --method POST --uri $URI --header Content-Type=application/json --body "$JSON"
-}
+#     az rest --method POST --uri $URI --header Content-Type=application/json --body "$JSON"
+# }
 
-USER_TYPE=$(az account show --query user.type -o tsv)
-if [ ${USER_TYPE} == "user" ]; then
-    echo "granting consent to logged in user"
-    az ad app permission admin-consent --id ${APPLICATION_ID}
+# USER_TYPE=$(az account show --query user.type -o tsv)
+# if [ ${USER_TYPE} == "user" ]; then
+#     echo "granting consent to logged in user"
+#     az ad app permission admin-consent --id ${APPLICATION_ID}
 
-    echo "Initializing state with user: $(az ad signed-in-user show --query userPrincipalName -o tsv)"
-else
-    echo "granting consent to logged in service principal" - Need to use the beta rest API for service principals. not supported by az cli yet
+#     echo "Initializing state with user: $(az ad signed-in-user show --query userPrincipalName -o tsv)"
+# else
+#     echo "granting consent to logged in service principal" - Need to use the beta rest API for service principals. not supported by az cli yet
 
-    ADGRAPHID=$(az ad sp show --id "${active_directory_graph_id}" --query "objectId" -o tsv)
+#     ADGRAPHID=$(az ad sp show --id "${active_directory_graph_id}" --query "objectId" -o tsv)
 
-    echo "grant consent (Application.ReadWrite.OwnedBy):"
-    grant_consent "${ADGRAPHID}" "${SP_ID}" "${active_directory_graph_resource_access_id_Application_ReadWrite_OwnedBy}"
+#     echo "grant consent (Application.ReadWrite.OwnedBy):"
+#     grant_consent "${ADGRAPHID}" "${SP_ID}" "${active_directory_graph_resource_access_id_Application_ReadWrite_OwnedBy}"
    
-    echo "\ngrant consent (Directory.Read.All):"
-    grant_consent "${ADGRAPHID}" "${SP_ID}" "${active_directory_graph_resource_access_id_Directory_ReadWrite_All}"
+#     echo "\ngrant consent (Directory.Read.All):"
+#     grant_consent "${ADGRAPHID}" "${SP_ID}" "${active_directory_graph_resource_access_id_Directory_ReadWrite_All}"
 
 
-    MSGRAPHID=$(az ad sp show --id "${microsoft_graph_id}" --query "objectId" -o tsv)   
+#     MSGRAPHID=$(az ad sp show --id "${microsoft_graph_id}" --query "objectId" -o tsv)   
 
-    echo "\ngrant consent (AppRoleAssignment.ReadWrite.All):"
-    grant_consent "${MSGRAPHID}" "${SP_ID}" "${microsoft_graph_AppRoleAssignment_ReadWrite_All}"
+#     echo "\ngrant consent (AppRoleAssignment.ReadWrite.All):"
+#     grant_consent "${MSGRAPHID}" "${SP_ID}" "${microsoft_graph_AppRoleAssignment_ReadWrite_All}"
 
-fi
-EOT
+# fi
+# EOT
 
-}
+# }
 
 
 resource "azuread_service_principal" "launchpad" {
