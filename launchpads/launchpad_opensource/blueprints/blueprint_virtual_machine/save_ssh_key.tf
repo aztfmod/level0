f0ce947ae8a9926bca11bb71f33b1e921844aa81
lolorol <lalesle@microsoft.com>
2020-04-07 10:17:54 +0000

    Remove ssh key from kv

--- launchpads/launchpad_opensource/blueprints/blueprint_virtual_machine/save_ssh_key.tf
+++ launchpads/launchpad_opensource/blueprints/blueprint_virtual_machine/save_ssh_key.tf
@@ -1,27 +1,27 @@
-data "azurerm_key_vault_secret" "private_key_pem" {
-  name         = module.vm.ssh_private_key_pem_secret_id.name
-  key_vault_id = module.vm.ssh_private_key_pem_secret_id.key_vault_id
-}
+# data "azurerm_key_vault_secret" "private_key_pem" {
+#   name         = module.vm.ssh_private_key_pem_secret_id.name
+#   key_vault_id = module.vm.ssh_private_key_pem_secret_id.key_vault_id
+# }
 
-locals {
-    save_ssh_key_cmd = <<EOT
-        cat <<EOF > ~/.ssh/${module.public_ip.ip_address}.private 
-${base64decode(data.azurerm_key_vault_secret.private_key_pem.value)}
-EOF
-    EOT
-}
+# locals {
+#     save_ssh_key_cmd = <<EOT
+#         cat <<EOF > ~/.ssh/${module.public_ip.ip_address}.private 
+# ${base64decode(data.azurerm_key_vault_secret.private_key_pem.value)}
+# EOF
+#     EOT
+# }
 
 
-resource "null_resource" "save_ssh_key" {
-    depends_on  = [module.vm]
-    count       = var.vm_object.save_ssh_private_key_pem == true ? 1 : 0
+# resource "null_resource" "save_ssh_key" {
+#     depends_on  = [module.vm]
+#     count       = var.vm_object.save_ssh_private_key_pem == true ? 1 : 0
 
-    provisioner "local-exec" {
-        command = "mkdir -p ~/.ssh"
-    }
+#     provisioner "local-exec" {
+#         command = "mkdir -p ~/.ssh"
+#     }
 
-    provisioner "local-exec" {
-        command = local.save_ssh_key_cmd
-    }
+#     provisioner "local-exec" {
+#         command = local.save_ssh_key_cmd
+#     }
 
-}
+# }
