f54f34fdf73acc234e393e2ade6efe48c5018fcc
lolorol <lalesle@microsoft.com>
2020-05-08 15:28:29 +0000

    Grant access to pipelines

--- bootstrap/step3-azure_devops_pipelines/pat.tf
+++ bootstrap/step3-azure_devops_pipelines/pat.tf
@@ -3,7 +3,7 @@ resource "azuredevops_variable_group" "pat" {
     project_id   = data.azuredevops_projects.project.projects.*.project_id[0]
     name         = "release-global-pat"
     description  = "Managed by Terraform"
-    allow_access = false
+    allow_access = true
 
     variable {
         name  = "TF_VAR_azure_devops_pat_token"
