dac16a569f538ac85de0ab6b48e5b663186a17bf
lolorol <lalesle@microsoft.com>
2020-05-12 12:57:48 +0000

    Add launchpad destroy to ci

--- launchpads/launchpad_opensource/identity_custom_role.tf
+++ launchpads/launchpad_opensource/identity_custom_role.tf
@@ -11,7 +11,7 @@ resource "azurerm_role_definition" "launchpad" {
       "Microsoft.Authorization/roleAssignments/read",
       "Microsoft.Authorization/roleAssignments/write",
       "Microsoft.Authorization/roleDefinitions/write",
-      "Microsoft.Authorization/roleDefinitions/delete",
+      "Microsoft.Authorization/roleDefinitions/delete"
     ]
 
   }
