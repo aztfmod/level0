a9b6e5e4ae281957e1b78f5fb4532b3e361fca9a
lolorol <lalesle@microsoft.com>
2020-05-13 02:16:50 +0000

    Fix diagnostics ci

--- launchpads/launchpad_opensource/diagnostics.tf
+++ launchpads/launchpad_opensource/diagnostics.tf
@@ -6,7 +6,7 @@ module "diagnostics" {
   name                  = var.resource_diagnostics_name
   convention            = var.convention
   resource_group_name   = azurerm_resource_group.rg_devops.name
-  prefix                = local.prefix
+  prefix                = local.prefix_start_alpha
   location              = azurerm_resource_group.rg_devops.location
   tags                  = local.tags
 
